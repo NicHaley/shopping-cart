61bd433d4487ff9c97786844c31c785cdcd046b1
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2015-05-13 21:14:05 -0400

    Set up Product model

--- shopping-cart/app/models/product.rb
+++ shopping-cart/app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ActiveRecord::Base
+	has_many :order_items
+
+	# default scope checks if the active flag is set to true. This ensures that deleted/inactive products aren't shown.
+	default_scope { where(active: true) }
 end
