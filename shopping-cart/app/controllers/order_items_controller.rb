a6cf0f0661d9f2da81ea34d0561b4d3c020380e7
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2015-05-13 22:18:58 -0400

    Set up orderItems controller

--- shopping-cart/app/controllers/order_items_controller.rb
+++ shopping-cart/app/controllers/order_items_controller.rb
@@ -1,10 +1,31 @@
 class OrderItemsController < ApplicationController
-  def create
-  end
+	before_filter :load_order
 
-  def update
-  end
+	def create
+		@order_item = @order.order_items.new(order_item_params)
+		@order.save
+		# Save the current order to the session
+		session[:order_id] = @order.id
+	end
 
-  def destroy
-  end
+	def update
+		@order_item = @order.order_items.find(params[:id])
+		@order_item.update_attributes(order_item_params)
+		@order_items = @order.order_items
+	end
+
+	def destroy
+		@order_item = @order.order_items.find(params[:id])
+		@order_item.destroy
+		@order_items = @order.order_items
+	end
+
+	private
+	def load_order
+		@order = current_order
+	end
+
+	def order_item_params
+		params.require(:order_item).permit(:quantity, :product_id)
+	end
 end
