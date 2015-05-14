11ce9bd52078f5f8c0283fa2787f5a676cd2ac85
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2015-05-13 22:21:31 -0400

    Set up products controller

--- shopping-cart/app/controllers/products_controller.rb
+++ shopping-cart/app/controllers/products_controller.rb
@@ -1,4 +1,7 @@
 class ProductsController < ApplicationController
-  def index
-  end
+	def index
+		@products = Product.all
+		# A new instance of orderitem is created to be used in our forms
+		@order_item = current_order.order_items.new
+	end
 end
