ff935ac151f38698c1b606666af2addf9e8a98d6
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2015-05-13 21:58:13 -0400

    Set up CartController

--- shopping-cart/app/controllers/application_controller.rb
+++ shopping-cart/app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :current_order
+
+  def current_order
+  	# If session[:order_id] is not nil, return the current session order, else create a new Order
+  	if !session[:order_id].nil?
+  		Order.find(session[:order_id])
+  	else
+  		Order.new
+  	end
+  end
 end
--- shopping-cart/app/controllers/carts_controller.rb
+++ shopping-cart/app/controllers/carts_controller.rb
@@ -1,4 +1,6 @@
 class CartsController < ApplicationController
   def show
+  	# Set @order_items equal to the current_order (defined in the application controller)
+  	@order_items = current_order.order_items
   end
 end
