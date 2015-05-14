2a99530e1e22fd669abbd2cfcca6ba4f998f5046
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2015-05-13 21:05:44 -0400

    Set up orderItems model

--- shopping-cart/app/models/order.rb
+++ shopping-cart/app/models/order.rb
@@ -1,20 +1,21 @@
 class Order < ActiveRecord::Base
-  belongs_to :order_status
-  has_many :order_items
-  before_create :set_order_status
-  before_save :update_subtotal
+	belongs_to :order_status
+	has_many :order_items
+	before_create :set_order_status
+	before_save :update_subtotal
 
-  def subtotal
-  	order_items.collect {|orderitem| orderitem.valid? (oi.quantity * oi.unit_price) : 0 }.sum
-  end
+	def subtotal
+		order_items.collect {|orderitem| orderitem.valid? (oi.quantity * oi.unit_price) : 0 }.sum
+	end
 
-  private
-  def set_order_status
+	private
+	def set_order_status
   	# Order_status_id = 1 is 'In Progress'
-    self.order_status_id = 1
+  	self.order_status_id = 1
   end
 
+  # Update the subtotal before saving based on the subtotal method above
   def update_subtotal
-    self[:subtotal] = subtotal
+  	self[:subtotal] = subtotal
   end
 end
--- shopping-cart/app/models/order_item.rb
+++ shopping-cart/app/models/order_item.rb
@@ -1,4 +1,41 @@
 class OrderItem < ActiveRecord::Base
-  belongs_to :product
-  belongs_to :order
+	belongs_to :product
+	belongs_to :order
+
+	validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
+	validate :product_present
+	validate :order_present
+
+	before_save :finalize
+
+	def unit_price
+  	# Persisted: Returns true if the record is persisted, i.e. it’s not a new record and it was not destroyed, otherwise returns false.
+  	if persisted?
+  		self[:unit_price]
+  	else
+  		product.price
+  	end
+  end
+
+  def total_price
+  	quantity * unit_price
+  end
+
+  private
+  def product_present
+  	if product.nil?
+  		errors.add(:product, "is not valid or is not active.")
+  	end
+  end
+
+  def order_present
+  	if order.nil?
+  		errors.add(:order, "is not a valid order.")
+  	end
+  end
+
+  def finalize
+  	self[:unit_price] = unit_price
+  	self[:total_price] = quantity * self[:unit_price]
+  end
 end
