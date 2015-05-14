b123156ede72531169b787a3d843433800baf2d7
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2015-05-13 20:57:55 -0400

    Set up order model

--- shopping-cart/app/models/order.rb
+++ shopping-cart/app/models/order.rb
@@ -1,3 +1,20 @@
 class Order < ActiveRecord::Base
   belongs_to :order_status
+  has_many :order_items
+  before_create :set_order_status
+  before_save :update_subtotal
+
+  def subtotal
+  	order_items.collect {|orderitem| orderitem.valid? (oi.quantity * oi.unit_price) : 0 }.sum
+  end
+
+  private
+  def set_order_status
+  	# Order_status_id = 1 is 'In Progress'
+    self.order_status_id = 1
+  end
+
+  def update_subtotal
+    self[:subtotal] = subtotal
+  end
 end
